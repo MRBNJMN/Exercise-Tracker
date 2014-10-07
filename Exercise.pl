f2d935c1217123493511be652245c5dedf144262
Ben Wheeler <b.a.wheeler@gmail.com>
2014-10-07 16:57:36 -0400

    Testing, debugging, and improvement
    * Tested, debugged, and improved many issues, including:
    ** Initialization
    ** storeDate capture
    ** Getopt::Long functionality
    ** Usage cases
    ** use Exercise (module) vs. include 'Exercise.pl' (switched back to Library)
    ** Object creation
    ** DB creation, connection, and disconnection
    ** Table creation from Perl
    ** Data storage from Perl

---
+++ Exercise.pl
@@ -0,0 +1,99 @@
+package Exercise;
+
+# Constructor (left generic for inheritance purposes)
+sub new {
+	my $class = shift;
+	# $objRef is a reference to an anonymous hash, passed to the constructor
+	# by the program based on command line variables specified by the user.
+	# Keys: 
+	my $objRef = shift;
+
+	bless $objRef, $class;
+	return $objRef;
+}
+
+# Establishes a connection to the DB
+sub connectDB {
+	my $self = shift;
+	my $DB = shift;
+	my $user = shift;
+	my $pass = shift;
+
+	# Connect to DB
+	$self->{'DBH'} = DBI->connect($DB, $user, $pass, {
+		'PrintError' => 1, # Warnings
+		'RaiseError' => 1, # Dies
+	}) or die "Can't connect to DB $DB: $DBI::errstr";
+}
+
+# Disconnects from the DB (for good housekeeping)
+sub disconnectDB {
+	my $self = shift;
+	
+	# Disconnect from DB
+	$self->{'rc'} = $self->{'DBH'}->disconnect
+		or warn "Couldn't disconnect: $self->{'DBH'}->errstr";
+}
+
+# Stores the exercise in the DB according to date
+sub storeExercise {
+	my $self = shift;
+	my $date = shift;
+	my $statement; # Used to query the DB with interpolation available
+	my $sth; # Used as a statement handler
+
+	# Create the table if it doesn't exist
+	$statement =
+		"CREATE TABLE IF NOT EXISTS d_$date (logMy VARCHAR(64), weight INTEGER, reps INTEGER, distance INTEGER, time INTEGER)";
+	$sth = $self->{'DBH'}->prepare($statement);
+	$sth->execute();
+
+	# Parse $self->{} and store the results
+	$statement =
+		"INSERT INTO d_$date (logMy, weight, reps, distance, time) VALUES ('$self->{'logMy'}', '$self->{'weight'}', '$self->{'reps'}', '$self->{'distance'}', '$self->{'time'}')";
+	$sth = $self->{'DBH'}->prepare($statement);
+	$sth->execute();
+}
+
+#sub pullExerciseList {
+#	my $self = shift;
+#	my $date = shift;
+#	my $statement; # Used to query the DB with interpolation available
+#	my $sth; # Used as a statement handler
+#
+#	# Pull exercise names based on date
+#	$statement =
+#		"SELECT logMy
+#		FROM $date";
+#	$sth = $self->{'DBH'}->prepare($statement);
+#	$sth->execute;
+#}
+#
+#sub pullExercise {
+#	my $self = shift;
+#	my $name = shift;
+#	my $date = shift;
+#	my $statement; # Used to query the DB with interpolation available
+#	my $sth; # Used as a statement handler
+#
+#	# Pull exercise specifics based on name and date
+#	$statement =
+#		"SELECT *
+#		FROM $date
+#		WHERE logMy = $name";
+#	$sth = $self->{'DBH'}->prepare($statement);
+#	$sth->execute;
+#
+#	# Process the query
+#	while ( my ($logMyPull, $weightPull, $repsPull, $distancePull, $timePull) =  $sth->fetchrow_array() ) {
+#		print
+#		"logMy: $logMyPull\n
+#		weight: $weightPull\n
+#		reps: $repsPull\n
+#		distance: $distancePull\n
+#		time: $timePull\n
+#		\n"
+#	};
+#}
+#
+1;
--- exerciseTracker.pl
+++ exerciseTracker.pl
@@ -16,40 +16,45 @@ use 5.010;
 use strict;
 use warnings;
 
-use GetOpt::Long;
+use Getopt::Long;
 use DBI;
-use Exercise;
+require 'Exercise.pl';
 
 # To be used with GetOpt::Long to accept command line parameters
 my ($logMy, $weight, $reps, $distance, $time, $seeMy, $onDate);
 
 # To be used for database connection
 my ($DB, $user, $pass) = 
-	("exerciseTracker", "exerciseUser", "ax4GuXhXjKxvj76F");
+	("dbi:mysql:exerciseTracker", "exerciseUser", "ax4GuXhXjKxvj76F");
 
 # To be used for storage by date
-my ($sec, $min, $hour, $mday, $mon, $year, $wday, $yday, $isdst) =
-	localtime(time);
-my $storeDate = $year + 1900 . $mon + 1 . $mday;
+# sprintf ensures consistency with leading zeroes
+my ($sec, $min, $hour, $mday, $mon, $year, $wday, $yday, $isdst) = localtime(time);
+my $storeDate = sprintf ("%d%02d%02d", $year + 1900, $mon + 1, $mday);
 
 # Pull options from the command line
 GetOptions (
-	'l|logMy=s' = \$logMy,
-	'w|weight=i' = \$weight,
-	'r|reps=i' = \$reps,
-	'd|distance=i' = \$distance,
-	't|time=i' = \$time,
-	's|seeMy=s' = \$seeMy,
-	'o|onDate=i' = \$onDate,
+	'l|logMy=s' => \$logMy,
+	'w|weight=f' => \$weight,
+	'r|reps=i' => \$reps,
+	'd|distance=f' => \$distance,
+	't|time=f' => \$time,
+	's|seeMy=s' => \$seeMy,
+	'o|onDate=i' => \$onDate,
 );
 
+die "Usage error\n" if (
+	not ($logMy || $onDate)
+	or ($logMy && ($onDate || $seeMy))
+	or ($onDate && ($logMy || $weight || $reps || $distance || $time)));
+
 # Create an Exercise object
-my $exercise = new->Exercise({
-	'logMy' = $logMy,
-	'weight' = $weight,
-	'reps' = $reps,
-	'distance' = $distance,
-	'time' = $time,
+my $exercise = Exercise->new({
+	'logMy' => $logMy,
+	'weight' => $weight,
+	'reps' => $reps,
+	'distance' => $distance,
+	'time' => $time,
 });
 
 # Connect to the DB
