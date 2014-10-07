8f34fe9411d491d280cd8d0f1695ad224292f851
Ben Wheeler <b.a.wheeler@gmail.com>
2014-10-07 10:13:31 -0400

    Minor changes to initial code * Changed name of hash passed to new() to objRef * Added $self variable to methods for invocant

--- Exercise.pl
+++ Exercise.pl
@@ -3,17 +3,19 @@ package Exercise;
 # Constructor (left generic for inheritance purposes)
 sub new {
 	my $class = shift;
-	# $self is a reference to an anonymous hash, passed to the constructor
+	# $objRef is a reference to an anonymous hash, passed to the constructor
 	# by the program based on command line variables specified by the user.
 	# Keys: name (required), weight, reps, sets, time, distance
-	my $self = shift;
+	my $objRef = shift;
 
-	bless $self, $class;
-	return $self;
+	bless $objRef, $class;
+	return $objRef;
 }
 
 # Establishes a connection to the DB
 sub connectDB {
+	my $self = shift;
+	
 	my $DB = shift;
 	my $user = shift;
 	my $pass = shift;
@@ -24,12 +26,16 @@ sub connectDB {
 
 # Disconnects from the DB (for good housekeeping)
 sub disconnectDB {
+	my $self = shift;
+	
 	# Disconnect from DB
 	# This will happen at the end of the program
 }
 
 # Stores the exercise in the DB according to date
 sub storeExercise {
+	my $self = shift;
+	
 	my $date = shift;
 	my $exercise = shift; # Reference to object
 
@@ -37,12 +43,16 @@ sub storeExercise {
 }
 
 sub pullExerciseList {
+	my $self = shift;
+	
 	my $date = shift;
 
 	# Pull exercise names based on date
 }
 
 sub pullExercise {
+	my $self = shift;
+
 	my $name = shift;
 	my $date = shift;
 
